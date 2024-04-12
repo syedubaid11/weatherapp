553d24b4d6a4651d4b1e8ee601286c459e5b13ea
syedubaid <syedabuubaid@gmail.com>
2024-04-12 22:34:28 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 const submitcityname=document.getElementById('submitcityname');
 const cityname=document.getElementById('cityname');
-const apiKey='3903e98485bd41ca831171844240804';
+const apiKey='5f3744c6037d48dd9f2161349241204';
 
-submitcityname.addEventListener('click',()=>{
-    
+
+submitcityname.addEventListener('click',(e)=>{
+    e.preventDefault();
+    console.log("in process")
     fetchApi();
+
 })
-
-
 
 
 async function fetchApi(){
     const city=cityname.value;
-    const url=`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+    const url=`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
     try{
-    const response=await fetch(url);
-    const data=await response.json();
-    console.log(data)
-    console.log("succesfully fetched")
+        const response=await fetch(url);
+        const data=await response.json();
+        console.log(data)
+        console.log("succesfully fetched")
     }
     catch(error){
-        console.log("Error Fethching api data",error);
+        console.log("Error Fetching API data",error);
         return null;
 
     }
 
 }
+
+
+
 
 
